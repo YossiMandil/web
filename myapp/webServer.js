15b3d77dfaf80226e9fa85cc13982f1790b61668
YossiMandil <yossimandil.com>
2018-07-17 16:11:19 +0300

    web server is working!!!!!!!

--- myapp/webServer.js
+++ myapp/webServer.js
@@ -11,9 +11,14 @@ app.use(express.static(__dirname + '/public'))
 
 app.post('/calculate', function(req, res) {
   console.log('shit just got real');
-
   console.log(req.body);
-  res.send(req.body);
+  //todo checkout if calculate next step gets json or string
+  var s = req.body.calculatorState;
+  if (s!=null){
+      s = JSON.stringify(s);
+  }
+  res.send(web.calculateNextState(s,
+    req.body.input));
 
 })
 
